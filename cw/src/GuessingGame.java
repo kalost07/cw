10a490e8b3e2f5c51b32922db61ff07b8239f7bd
kalost07 <108670187+kalost07@users.noreply.github.com>
2024-10-25 13:24:14 +0300

    Add GuessingGame 2

--- cw/src/GuessingGame.java
+++ cw/src/GuessingGame.java
@@ -1,28 +1,36 @@
 import java.util.Scanner;
 
 public class GuessingGame {
-    static int ans = 5;
 
-    static boolean guess(int attempt) {
-        Scanner reader = new Scanner(System.in);
-        System.out.print("Enter a number: ");
-        int guess = reader.nextInt();
-        if(guess == ans) {
-            System.out.println("Congratulations!");
-            return true;
-        }
-        else {
-            if(guess < ans) System.out.println("Incorrect. Your guess is smaller than the answer");
-            else System.out.println("Incorrect. Your guess is greater than the answer");
-            System.out.println("You have " + attempt + " guesses left.\n");
-        }
-        return false;
-    }
-    public static void main(String[] args) {
-        if (guess(2)) return;
-        if (guess(1)) return;
-        if (guess(0)) return;
+    public static void main(String args[]) {
+        Scanner read = new Scanner(System.in);
+        int ans = (int)(Math.random() * 100) + 1;
+        System.out.println("Welcome to guessing game.\n I have generated a random number between 1 and 100. You have to guess what it is.\n I will tell you whether it's lower or higher than the answer.\n You may quit by typing 'quit'.\n");
 
-        System.out.println("You are out of guesses");
+        int attempts = 0;
+        while(true) {
+            System.out.println("Enter your guess");
+            if (read.hasNextInt()) {
+                int guess = read.nextInt();
+                if(guess == ans) {
+                    System.out.println("You guessed correctly in " + ++attempts + " attempts!");
+                    return;
+                }
+                else {
+                    if(guess < ans) System.out.println("The answer is higher. You have made " + ++attempts + " attempts.");
+                    else System.out.println("The answer is lower. You have made " + ++attempts + " attempts.");
+                }
+            }
+            else {
+                String input = read.nextLine();
+                if (input.equals("quit")) {
+                    System.out.println("You have quit after " + attempts + " attempts. The answer was " + ans);
+                    return;
+                }
+                else {
+                    System.out.println("Please enter an integer.");
+                }
+            }
+        }
     }
 }
